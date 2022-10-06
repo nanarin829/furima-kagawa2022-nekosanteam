77b2d4de43c6699f5f980f702c3cdc684719225b
masatoso <soffy22546@gmail.com>
2022-10-06 15:08:15 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day, format: { with: /\A[０-９]+\z/ }
+
 end
