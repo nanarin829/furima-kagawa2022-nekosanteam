de513614fdf4ab8e2d83a06012333e96a2c6991d
masatoso <soffy22546@gmail.com>
2022-10-07 20:07:16 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :last_name, presence: true
   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, presence: true
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name_kana, presence: true
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
