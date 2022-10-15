3405b29de2d6f07a366970af5feb4bf85ad8ffdb
dancingsoul <keisukemelonhouse@gmail.com>
2022-10-15 21:36:06 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, only: [:show, :edit, :move_to_index]
   before_action :move_to_index, only: [:edit]
-  
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -30,7 +29,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-    redirect_to action: :show
+      redirect_to action: :show
     else
       render :edit
     end
@@ -48,10 +47,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user.id
-    redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user.id
   end
-
-
 end
