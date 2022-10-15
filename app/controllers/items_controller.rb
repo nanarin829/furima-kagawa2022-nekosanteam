0a623e31bf543f1379126e2570a5042ec7995f6e
dancingsoul <keisukemelonhouse@gmail.com>
2022-10-15 21:30:07 +0900

    リファクタリング（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit, :move_to_index]
+  before_action :move_to_index, only: [:edit]
+  
 
   def index
     @items = Item.all.order('created_at DESC')
