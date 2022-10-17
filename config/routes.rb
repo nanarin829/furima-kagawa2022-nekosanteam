3c41ada04b0691361feb25389af87888463e5e7a
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-10-17 20:06:08 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items do
+    member do
+      get 'orders'
+    end
+  end
 end
