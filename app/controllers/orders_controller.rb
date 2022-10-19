69598db017aa24f67eaeba8507c6f6b062665823
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-10-19 21:16:26 +0900

    #createの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,13 @@ class OrdersController < ApplicationController
     @items = Item.all
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
 end
